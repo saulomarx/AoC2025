3de946399978066649451e7abdf2765a762ce56c
Saulo Marx de Paiva <saulo.marx@hotmail.com>
2025-12-10 14:18:28 -0300

    feat: add part 1 for day 01

---
+++ internal/days/day01/day01.go
@@ -0,0 +1,41 @@
+package day01
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Part01(input []string) {
+	sum := 50
+	countZero := 0
+	for _, line := range input {
+		d, v := getDirectionAndValue(line)
+		// min 0
+		if d == "L" {
+			sum -= v%100
+			if sum < 0 {
+				sum += 100
+			}
+		}
+		// max 99
+		if d == "R" {
+			sum += v%100
+			if sum > 99 {
+				sum -= 100
+			}
+		}
+
+		if sum == 0 {
+			countZero++
+		}
+
+	}
+	fmt.Println(countZero)
+}
+
+func getDirectionAndValue(s string) (direction string, value int) {
+	direction = s[:1]
+	v, _ := strconv.Atoi(s[1:])
+	value = v
+	return
+}
